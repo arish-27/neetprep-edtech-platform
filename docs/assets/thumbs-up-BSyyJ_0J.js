e63174ba8b4d8a27b0b3edcc6a2c62c04f4fbda7
arish-27 <220801027@rajalakshmi.edu.in>
2026-08-20 22:30:57 +0530

    fix: add smart demo fallback authentication and offline data support for GitHub Pages

---
+++ docs/assets/thumbs-up-BSyyJ_0J.js
@@ -0,0 +1,11 @@
+import{b as a}from"./index-B-G-kidW.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z",key:"1lielz"}]],c=a("MessageSquare",e);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M7 10v12",key:"1qc93n"}],["path",{d:"M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z",key:"emmmcr"}]],t=a("ThumbsUp",s);export{c as M,t as T};
